26ba644c614f03a7b1c6f0ad5e724be9bd62fc79
Ricardo Lima <luizricardodelima9@gmail.com>
2022-01-30 23:59:19 +0100

    Users apis

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+let users = [];
+
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+app.post('/api/users', (req, res) => {
+  let user = {_id: users.length, username: req.body.username};
+  users.push(req.body.username);
+  res.json(user);
+});
+
+app.get('/api/users', (req, res) => {
+  res.json(users.map((username, index) => {return {_id: index, username: username}}));
+});
+
 
 
 
