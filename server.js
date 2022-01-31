f71aece43e4dd9b91c9220d6206fce037fed1be0
Ricardo Lima <luizricardodelima9@gmail.com>
2022-01-31 01:01:07 +0100

    More implementation

--- server.js
+++ server.js
@@ -5,10 +5,23 @@ const cors = require('cors')
 require('dotenv').config()
 
 let users = [];
+let exercises = [];
 
 app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// Logger middleware
+app.use((req, res, next) => {
+  console.log('--------------------------------------------------------------------------------------');
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  console.log(`params:`);
+  console.log(req.params);
+  console.log(`body:`);
+  console.log(req.body);
+  console.log('--------------------------------------------------------------------------------------');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -23,6 +36,63 @@ app.post('/api/users', (req, res) => {
 app.get('/api/users', (req, res) => {
   res.json(users.map((username, index) => {return {_id: index, username: username}}));
 });
+
+app.post('/api/users/:uid/exercises', (req, res) => {
+  let index = Number.parseInt(req.params.uid);
+  if (isNaN(index))
+    res.json({ error: 'Invalid user id'});
+  else if (index >= users.length)
+    res.json({ error: 'User not found'});
+  else if (req.body.description === undefined || req.body.description == '')
+    res.json({ error: 'Invalid description'});
+  else if (req.body.duration === undefined || isNaN(Number(req.body.duration)))
+    res.json({ error: 'Invalid duration'});
+  else if (req.body.date && isNaN(Date.parse(req.body.date)))
+    res.json({ error: 'Invalid date'});
+  else {
+    let exercise = {
+      _id: index,
+      description: req.body.description,
+      duration: Number(req.body.duration),
+      date: req.body.date ? Date.parse(req.body.date) : new Date().getTime()
+    };
+    exercises.push(exercise);
+    // Formatting exercise to output
+    exercise.date = new Date(exercise.date).toDateString();
+    exercise.username = users[index];
+    res.json(exercise);
+  }
+});
+
+app.get('/api/users/:uid/logs?', (req, res) => {
+  let index = Number.parseInt(req.params.uid);
+  if (isNaN(index))
+    res.json({ error: 'Invalid user id'});
+  else if (index >= users.length)
+    res.json({ error: 'User not found'});
+  else {
+    let result = [];
+    exercises.forEach((exercise) => {
+      if (exercise._id == index)
+        result.push(exercise);
+    });
+
+    // TODO formatting and other fields
+    /* {
+      username: "fcc_test",
+      count: 1,
+      _id: "5fb5853f734231456ccb3b05",
+      log: [{
+        description: "test",
+        duration: 60,
+        date: "Mon Jan 01 1990",
+      }]
+    } */
+
+    res.json(result);
+  }
+});
+
 
 
 
